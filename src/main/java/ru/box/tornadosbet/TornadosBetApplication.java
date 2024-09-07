4c729ba135f8e3c72073a6762e48f25c28c8dfc3
fffff154 <nalekseev.154@gmail.com>
2024-09-08 01:12:40 +0300

    tornadosBet version 0.01

---
+++ src/main/java/ru/box/tornadosbet/TornadosBetApplication.java
@@ -0,0 +1,13 @@
+package ru.box.tornadosbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TornadosBetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TornadosBetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/box/tornadosbet/TornadosBetApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.box.tornadosbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TornadosBetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
